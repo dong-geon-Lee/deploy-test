ddc57c01463dd99880a62d5824c49b1896c92391
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-11-29 15:45:43 +0900

    이런 경우가 있구나

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <h2>배포 방향은 Git page로</h2>
-      <h3>이거 되는거 맞나 ??</h3>
+      <h2>실화인가??</h2>
     </div>
   );
 };
